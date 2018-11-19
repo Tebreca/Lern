5351c5e8b9de76ebb8232127be9f47c3112d5266
Tebreca <ewan.arends10@gmail.com>
2018-11-19 07:14:32 +0100

    Add project files.

---
+++ Learn-Test/Words.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Word
+{
+    private string original;
+    private string translation;
+
+	public Word(string original, string translation)
+	{
+        this.Original = original;
+        this.Translation = translation;
+	}
+
+    public string Original { get => original; set => original = value; }
+    public string Translation { get => translation; set => translation = value; }
+
+    public override bool Equals(object obj)
+    {
+        var word = obj as Word;
+        return word != null &&
+               Original == word.Original &&
+               Translation == word.Translation;
+    }
+
+    public override int GetHashCode() => HashCode.Combine(Original, Translation);
+
+    public Word Swap() => new Word(Translation, Original);
+}
+
+public class WordList
+{
+
+}
